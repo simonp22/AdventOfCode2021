e76f7493e5c5c03db3b23d610d0411d4b65ed7a8
Simon Phillips <simonp@corp.icukhosting.co.uk>
2021-12-10 15:47:23 +0000

    Day 2, 3 Start of Day 4

---
+++ Day2/Program.cs
@@ -0,0 +1,58 @@
+﻿// Read file
+var fileLines = File.ReadAllLines("input.txt").ToList();
+
+Part2();
+
+void Part1() {
+    int horizontalPos = 0;
+    int depth = 0;
+
+    foreach (var line in fileLines) {
+        string direction = line.Split(' ')[0];
+        int movement = Convert.ToInt32(line.Split(' ')[1]);
+        switch (direction) {
+            case "forward":
+                horizontalPos += movement;
+                break;
+            case "down":
+                depth += movement;
+                break;
+            case "up":
+                depth -= movement;
+                break;
+        }
+    }
+
+    Console.WriteLine("Horizontal: " + horizontalPos);
+    Console.WriteLine("Depth: " + depth);
+    Console.WriteLine("Multiplied: " + (depth * horizontalPos));
+    Console.ReadLine();
+}
+
+void Part2() {
+    int horizontalPos = 0;
+    int depth = 0;
+    int aim = 0;
+
+    foreach (var line in fileLines) {
+        string direction = line.Split(' ')[0];
+        int movement = Convert.ToInt32(line.Split(' ')[1]);
+        switch (direction) {
+            case "forward":
+                horizontalPos += movement;
+                depth += (aim * movement);
+                break;
+            case "down":
+                aim += movement;
+                break;
+            case "up":
+                aim -= movement;
+                break;
+        }
+    }
+
+    Console.WriteLine("Horizontal: " + horizontalPos);
+    Console.WriteLine("Depth: " + depth);
+    Console.WriteLine("Multiplied: " + (depth * horizontalPos));
+    Console.ReadLine();
+}
---
+++ Day3/Program.cs
@@ -0,0 +1,55 @@
+﻿// Read file
+List<string> fileLines = File.ReadAllLines("day3.txt").ToList();
+    int binaryLength = fileLines[0].Length;
+
+Part1();
+Part2();
+
+void Part1() {
+    int[] gamma = new int[binaryLength];
+    int[] epsilon = new int[binaryLength];
+
+    for (int i = 0; i < binaryLength; i++) {
+        gamma[i] = fileLines.Where(x => x[i] == '1').Count() > (fileLines.Count() / 2) ? 1 : 0;
+    }
+
+    for (int i = 0; i < binaryLength; i++) {
+        epsilon[i] = fileLines.Where(x => x[i] == '1').Count() < (fileLines.Count() / 2) ? 1 : 0;
+    }
+
+    int gammaDec = Convert.ToInt32(string.Join("", gamma), 2);
+    int epsilonDec = Convert.ToInt32(string.Join("", epsilon), 2);
+
+    int power = gammaDec * epsilonDec;
+    Console.WriteLine("Power: " + power);
+}
+
+void Part2() {
+    int[] oxygen = new int[binaryLength];
+    List<string> oxygenList = fileLines.ToList();
+
+    for (int i = 0; i < binaryLength; i++) {
+        oxygen[i] = oxygenList.Where(x => x[i] == '1').Count() >= oxygenList.Where(x => x[i] == '0').Count() ? 1 : 0;
+        oxygenList.RemoveAll(x => x[i].ToString() != oxygen[i].ToString());
+    }
+
+    int oxygenRating = Convert.ToInt32(oxygenList[0], 2);
+    Console.WriteLine("Oxygen: " + oxygenRating);
+
+
+    int[] scrubber = new int[binaryLength];
+    List<string> scrubberList = fileLines.ToList();
+
+    for (int i = 0; i < binaryLength; i++) {
+        scrubber[i] = scrubberList.Where(x => x[i] == '1').Count() < scrubberList.Where(x => x[i] == '0').Count() ? 1 : 0;
+        scrubberList.RemoveAll(x => x[i].ToString() != scrubber[i].ToString());
+        if (scrubberList.Count() == 1) {
+            break;
+        }
+    }
+
+    int coRating = Convert.ToInt32(scrubberList[0], 2);
+    Console.WriteLine("CO2: " + coRating);
+
+    Console.WriteLine("Life support: " + (coRating * oxygenRating));
+}
